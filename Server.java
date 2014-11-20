28606538e7220cd3c0df65dbfb8ec7d5c39e5d72
Colin Ashburn <cashburn@purdue.edu>
2014-11-20 11:22:37 -0500

    2014-11-20

--- Server.java
+++
@@ -1,6 +0,0 @@
-public class Server {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ServerSocket serverSocket = new ServerSocket(4242);
-        Socket socket = serverSocket.accept();
-    }
-}
